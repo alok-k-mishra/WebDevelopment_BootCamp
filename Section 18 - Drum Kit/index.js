3de055a3cb586ac25f22c1d13a6ae08d1d696c5e
Alok <mail.alokkumarmishra2004.com>
2024-11-21 11:40:23 +0000

    drum kit mouse click event

---
+++ Section 18 - Drum Kit/index.js
@@ -0,0 +1,44 @@
+drumButtons = document.querySelectorAll(".drum").length;
+
+
+var kick = new Audio("sounds/kick-bass.mp3");
+var crash = new Audio("sounds/crash.mp3");
+var snare = new Audio("sounds/snare.mp3");
+var tom1 = new Audio("sounds/tom-1.mp3");
+var tom2 = new Audio("sounds/tom-2.mp3");
+var tom3 = new Audio("sounds/tom-3.mp3");
+var tom4 = new Audio("sounds/tom-4.mp3");
+
+for (var i = 0; i < drumButtons; i++) {
+    document.querySelectorAll(".drum")[i].addEventListener("click", function() {
+        var buttonInnerHTML = this.innerHTML;
+
+        switch (buttonInnerHTML) {
+            case "w":
+                kick.play();
+                break;
+            case "a":
+                crash.play();
+                break;
+            case "s":
+                snare.play();
+                break;
+            case "d":
+                tom1.play();
+                break;
+            case "j":
+                tom2.play();
+                break;
+            case "k":
+                tom3.play();
+                break;
+            case "l":
+                tom4.play();
+                break;
+        
+            default:
+                console.log();
+        }
+
+    });
+}
