0f9b22d8b0fffdd571abb0ecb3a1a9acffe19d2f
Alok <mail.alokkumarmishra2004.com>
2024-11-22 11:36:08 +0000

    section 19 complete

--- Section 19 - jQuery/index.js
+++ Section 19 - jQuery/index.js
@@ -1,1 +1,21 @@
-$("h1").css("color", "red");
+
+
+
+
+
+
+
+// $("button").click(function() {
+//   $("h1").css("color", "purple");
+// });
+
+
+
+// $(document).keypress(function(event) {
+//   $("h1").text(event.key);
+// });
+
+
+// $("h1").on("mouseover", function() {
+//     $("h1").css("color", "purple");
+// });
